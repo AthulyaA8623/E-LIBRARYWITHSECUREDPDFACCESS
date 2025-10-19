a64523344d8a9622ba51524a2104375450b2d03e
athulyaanith <athulyan8625@gmail.com>
2025-10-19 14:41:59 +0530

    initial commit

--- E-library/E-libraryBackend/server.js
+++ E-library/E-libraryBackend/server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -7,15 +6,8 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-//atlas 
 
-// const dotenv = require('dotenv');
-// const connectDB = require('./config/database');
 
-// dotenv.config();
-// connectDB();
-
-// Middleware
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
